916bbe090c699e03741a2f2507d6fa5d5c8589e7
VedMakes <singhvedant001@gmail.com>
2026-01-22 18:20:56 +0530

    commit:changed voicerec model

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ SILENCE_TIMEOUT_MS = 1000
 
 WAKE_WORDS = ["jarvis"]
 
-VOSK_MODEL_PATH = "model/vosk-model-small-en-in-0.4/vosk-model-small-en-in-0.4"
+VOSK_MODEL_PATH = "model/vosk-model-hi-0.22/vosk-model-hi-0.22"
